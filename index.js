9a0d1d315052221210604545ae8c33adbb08f335
butlern-mn <109308507+butlern-mn@users.noreply.github.com>
2023-04-03 09:52:57 -0500

    MyPortfolio Repo Started
    Copied everything from V2_Challenge 19, where I had been developing my portfolio, over to own repository with MyPortfolio name for publishing.
    Reconnected all the images/videos.

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Is running")
+
+$(document).ready(function(){
+  $(".contactButton").hover(function(){
+    $(this).css("background-color", "#013863");
+    $(this).css("font-size", "32px");
+    }, function(){
+    $(this).css("background-color", "#007b84");
+    $(this).css("font-size", "24px");
+  });
+});
