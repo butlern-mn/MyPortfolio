58c40582fd11bfcc914e0a9a8fc648169271963d
butlern-mn <109308507+butlern-mn@users.noreply.github.com>
2023-04-10 13:39:43 -0500

    Proofread Update
    Verified language of Home page, and updated links of Resume, Linked In.  Uploaded new design resume to Drive.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Is running")
 $(document).ready(function(){
   $(".contactButton").hover(function(){
     $(this).css("background-color", "#013863");
-    $(this).css("font-size", "32px");
+    $(this).css("font-size", "28px");
     }, function(){
     $(this).css("background-color", "#007b84");
     $(this).css("font-size", "24px");
