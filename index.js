78ac2e0c1cb65ed3aaaf2fe879d4d305d5e5fc57
butlern-mn <109308507+butlern-mn@users.noreply.github.com>
2023-05-25 22:40:23 -0500

    Modal
    Attempt to add my modal when first land on page.

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ $(document).ready(function(){
     $(this).css("font-size", "24px");
   });
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("mymodalButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+document.getElementById("mymodalbutton").onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
